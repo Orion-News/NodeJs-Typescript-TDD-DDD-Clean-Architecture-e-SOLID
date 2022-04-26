02cac59ed77886740d78e25d17b5f77a3d2c371f
Orion-News <geeh-eb@Hotmail.com>
2022-04-26 14:56:55 -0300

    feat: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+  constructor() {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return new Promise(
+      res => res(
+        badRequest(new MissingParamError('email'))
+      )
+    )
+  }
+}
