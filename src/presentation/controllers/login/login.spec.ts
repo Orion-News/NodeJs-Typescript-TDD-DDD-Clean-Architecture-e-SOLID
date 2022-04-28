59af7386fbbde1259f34b3e714dfc5592586395a
Orion-News <geeh-eb@Hotmail.com>
2022-04-28 10:11:22 -0300

    test: ensure LoginController returns 200 if valid crendetials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { Authentication, EmailValidator, HttpRequest } from "./login-protocols";
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError, unauthorized } from "../../helpers/http-helper";
+import { badRequest, ok, serverError, unauthorized } from "../../helpers/http-helper";
 import { LoginController } from "./login";
 
 const makeEmailValidator = (): EmailValidator => {
@@ -44,7 +44,6 @@ const makeSut = (): SutTypes => {
         authenticationStub
    }
 }
-
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async() => {
@@ -109,5 +108,21 @@ describe('Login Controller', () => {
     
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
+  })
+
+  test('Should return 500 if Authentication throws', async() => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+    
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 if valid credential are provided', async() => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 })
