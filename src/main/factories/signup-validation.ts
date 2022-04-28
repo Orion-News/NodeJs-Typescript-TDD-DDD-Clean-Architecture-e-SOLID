208ccc83a90eb63ae8303e779705b78ee07558bb
Orion-News <geeh-eb@Hotmail.com>
2022-04-28 16:10:58 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -2,6 +2,8 @@ import { ValidationComposite } from '../../presentation/helpers/validatiors/vali
 import { RequeredFieldValidation } from '../../presentation/helpers/validatiors/required-field-validation'
 import { Validation } from '../../presentation/helpers/validatiors/validation'
 import { CompareFieldValidation } from '../../presentation/helpers/validatiors/compare-field-validation'
+import { EmailValidation } from '../../presentation/helpers/validatiors/email-validation'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
     const validations: Validation[] = []
@@ -9,5 +11,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
         validations.push(new RequeredFieldValidation(field))
     }
     validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
     return new ValidationComposite(validations)
 }
