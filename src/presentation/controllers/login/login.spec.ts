681f8005869c33c1f1c17450d62a3f691ca9d657
Orion-News <geeh-eb@Hotmail.com>
2022-04-29 11:05:50 -0300

    refactor: chance Authentication to receive a model

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,11 +1,11 @@
-import { Authentication,  HttpRequest, Validation } from "./login-protocols";
+import { Authentication,  AuthenticationModel,  HttpRequest, Validation } from "./login-protocols";
 import { MissingParamError } from "../../errors";
 import { badRequest, ok, serverError, unauthorized } from "../../helpers/http/http-helper";
 import { LoginController } from "./login";
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (email: string, password: string): Promise<string> {
+    async auth (authentication: AuthenticationModel): Promise<string> {
       return new Promise(resolve => resolve('any_token'))
     }
   }
@@ -52,7 +52,10 @@ describe('Login Controller', () => {
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     
     await sut.handle(makeFakeRequest())
-    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com.br', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com.br', 
+      password: 'any_password'
+    })
   })
 
   test('Should return 401 if invalid credential are provided', async() => {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -17,8 +17,10 @@ export class LoginController implements Controller {
         return badRequest(error)
       }
       const { email, password } = httpRequest.body
-      const accessToken = await this.authentication.auth(email, password)
-      console.log('dfebuig: ', accessToken)
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      })
       if(!accessToken) {
         return unauthorized()
       }
