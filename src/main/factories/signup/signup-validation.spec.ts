569a91b8797221334b173916b7346c2f568980ac
Orion-News <geeh-eb@Hotmail.com>
2022-04-29 10:25:45 -0300

    refactor: change path local of file to folder

---
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -0,0 +1,31 @@
+import { makeSignUpValidation } from './signup-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite/validation-composite'
+import { RequeredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation/required-field-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { CompareFieldValidation } from '../../../presentation/helpers/validators/compare-field-validation/compare-field-validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation/email-validation'
+import { EmailValidator } from '../../../presentation/protocols/email-validator'
+
+jest.mock('../../../presentation/helpers/validators/validation-composite/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('SignUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeSignUpValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+        validations.push(new RequeredFieldValidation(field))
+    }
+    validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
---
+++ src/presentation/helpers/validators/compare-field-validation/compare-field-validation.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../../errors'
+import { CompareFieldValidation } from './compare-field-validation'
+
+const makeSut = (): CompareFieldValidation => {
+  return new CompareFieldValidation('field', 'fieldToCompare')
+}
+
+describe('CompareFields Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const error = makeSut().validate({ field: 'any_value',  fieldToCompare: 'wrong_value'})
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const error = makeSut().validate({ field: 'any_value',  fieldToCompare: 'any_value'})
+    expect(error).toBeFalsy()
+  })
+})
