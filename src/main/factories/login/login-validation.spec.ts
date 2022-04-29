e8ee7c006d69c4dfe919bebd9ea9d1a5ff4f054f
Orion-News <geeh-eb@Hotmail.com>
2022-04-29 10:41:44 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,29 @@
+import { makeLoginValidation } from './login-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite/validation-composite'
+import { RequeredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation/required-field-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation/email-validation'
+import { EmailValidator } from '../../../presentation/protocols/email-validator'
+
+jest.mock('../../../presentation/helpers/validators/validation-composite/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('LoginUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeLoginValidation()
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+        validations.push(new RequeredFieldValidation(field))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
