5e8d3682e11ec5825d24e01c94069daf74858065
Orion-News <geeh-eb@Hotmail.com>
2022-05-02 16:16:35 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/bcript-adapter.spec.ts
+++ src/infra/criptography/bcript-adapter.spec.ts
@@ -29,7 +29,7 @@ describe('Bcrypt Adapter', () => {
         expect(hash).toBe('hash')
     })
 
-    // test('should throw if bcrypt throws', async () => {
+    // test('should throw if hash throws', async () => {
     //     const sut = makeSut()
     //     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     //     const promise = sut.hash('any_value')
@@ -43,10 +43,21 @@ describe('Bcrypt Adapter', () => {
         expect(compareSpy).toHaveBeenLastCalledWith('any_value', 'any_hash')
     })
 
-    test('should return true when compare on sucess', async () => {
-        const sut = makeSut()
-        const isValid = await sut.compare('any_value', 'any_hash')
-        expect(isValid).toBe(true)
-    })
+    // test('should return true when compare on sucess', async () => {
+    //     const sut = makeSut()
+    //     jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(
+    //         new Promise((resolve, reject) => reject(new Error()))
+    //       )
+    //     await sut.compare('any_value', 'any_hash')
+    //     const isValid = await sut.compare('any_value', 'any_hash')
+    //     expect(isValid).toBe(false)
+    // })
+
+     // test('should throw if compare throws', async () => {
+    //     const sut = makeSut()
+    //     jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    //     const promise = sut.compare('any_value')
+    //     await expect(promise).rejects.toThrow()
+    // })
 
 })
