c849c713cdaecfeb6c57ffb59c61b2fee226c918
Orion-News <geeh-eb@Hotmail.com>
2022-05-04 16:17:39 -0300

    feat: add LoginFactory

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -3,5 +3,5 @@ import { makeSignUpController } from '../factories/signup/signup.factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
-    router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
