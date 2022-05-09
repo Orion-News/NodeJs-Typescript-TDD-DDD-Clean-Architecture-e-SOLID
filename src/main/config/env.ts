03e034886dca241240697a437d83a1f5a3e671f7
Orion-News <geeh-eb@Hotmail.com>
2022-05-09 15:37:39 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://curso-mango:Hf5GCCm5mcojO56a@curse-mango.a85oa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    port: process.env.PORT || 5050,
-    jwtSecret: process.env.JWT_SECRET || 'tj670==5H|5H==076jt'
-}
+  mongoUrl: process.env.MONGO_URL || "mongodb://mongo:27017/myFirstDatabase",
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || "tj670==5H|5H==076jt",
+};
